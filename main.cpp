859c3dbed236212a473c2afd7c46f0b880875764
ahmed khaled <112342196+ahmed74ca@users.noreply.github.com>
2024-11-28 13:01:05 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "include/logic_simulator.h"
+
+int main() {
+	std::string inputString;
+
+	do {
+		std::getline(std::cin, inputString);
+	} while (GateGenerator::parse_input_string(inputString));
+
+	return 0;
+}
